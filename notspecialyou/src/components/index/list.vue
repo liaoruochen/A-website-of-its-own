30d81ed1d2380d7b7362c0acfea1c892a81299b3
liaoruochen <920037556>
2018-07-03 18:29:42 +0800

    新增了index中的组件

---
+++ notspecialyou/src/components/index/list.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="box">
+        <ul class="list">
+            <li class="title">
+                <router-link to="/" class="type">
+                    <img src="@/assets/home1.png" alt="">
+                    <span>首页</span>
+                </router-link>
+            </li>
+            <li class="title">
+                <router-link to="/" class="type">
+                    <img src="@/assets/label1.png" alt="">
+                    <span>标签</span>
+                </router-link>
+            </li>
+            <li class="title">
+                <router-link to="/" class="type">
+                    <img src="@/assets/archives1.png" alt="">
+                    <span>归档</span>
+                </router-link>
+            </li>
+            <li class="title">
+                <router-link to="/" class="type">
+                    <img src="@/assets/people1.png" alt="">
+                    <span>关于</span>
+                </router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    .box {
+        width: 100%;
+    }
+
+    .list {
+        max-width: 250px;
+        min-width: 230px;
+        height: 60px;
+        /* background-color: red; */
+        margin: 0 auto;
+        /* margin-top: 20px; */
+        display: flex
+    }
+
+    .title {
+        display: inline-block;
+        flex: 1;
+    }
+
+    .type {
+        display: block;
+        height: 60px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .type img {
+        width: 20px;
+        height: 20px;
+    }
+
+    .type span {
+        font-size: 12px;
+    }
+</style>
---
+++ notspecialyou/src/components/index/title.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+       
+            <p class="title">
+                不特别的你
+            </p>
+            <list></list>
+    </div>
+</template>
+
+<script>
+    // import top from '@/components/top-navigation/top-navigation'
+    import list from "@/components/index/list"
+    export default {
+        components:{
+            list
+        }
+    };
+</script>
+
+<style scoped>
+
+    .title {
+        font-size: 28px;
+        text-align: center;
+        margin-top: 40vh;
+    }
+
+</style>
