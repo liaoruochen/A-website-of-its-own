4aedc5ea453dcff9256d46d6665371ff3ef16f82
keepAwayFrom404 <xff777@qq.com>
2018-07-03 23:52:31 +0800

    新建了footers组件

--- notspecialyou/src/App.vue
+++ notspecialyou/src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <div id="app">
+    <footers></footers>
   </div>
 </template>
 
 <script>
+import Footers from '@/components/Footers'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Footers
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- notspecialyou/src/router/index.js
+++ notspecialyou/src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Footers from '@/components/Footers'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  // routes: [
+  //   {
       
-    }
-  ]
+  //   }
+  // ]
 })
