2ed8f457c5d1381e9255f831fae9c9ff231eb033
liaoruochen <920037556>
2018-07-03 18:32:49 +0800

    微调

--- notspecialyou/src/views/index.vue
+++ notspecialyou/src/views/index.vue
@@ -1,13 +1,26 @@
 <template>
- 
+    <div class="box">
+        <middletitle></middletitle>
+    </div>
+
 </template>
 
 <script>
-export default {
+import title from "@/components/index/title";
 
-}
+export default {
+  components: {
+    middletitle: title
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+.box {
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  background: url("https://desk-fd.zol-img.com.cn/t_s1920x1080c5/g5/M00/0C/0D/ChMkJlnF0vKIPFlaANd0cSO7qRUAAguMgJ2LX4A13SJ836.jpg") no-repeat;
+  background-size: cover;
+}
 </style>
