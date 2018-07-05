bfb1ac252350b10b217056cf52823ab152ca9598
liaoruochen <920037556@qq.com>
2018-07-05 22:28:25 +0800

    增加了vuex

--- notspecialyou/src/main.js
+++ notspecialyou/src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+
 
 Vue.config.productionTip = false
 
@@ -11,5 +13,6 @@ new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  store
 })
