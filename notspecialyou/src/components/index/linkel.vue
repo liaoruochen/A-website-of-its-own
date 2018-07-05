3d9b1da1a2c2b19ef89dd937f06e0eac83f7b77c
liaoruochen <920037556@qq.com>
2018-07-05 09:31:15 +0800

    新增了链接栏组件

---
+++ notspecialyou/src/components/index/linkel.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ul>
+            <li><img src="@/assets/weibo.png" alt=""></li>
+            <li><img src="@/assets/juejin.png" alt=""></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
