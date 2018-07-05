643ee38a8ba2de93dbe2e49291f03e265a5a05f8
liaoruochen <920037556@qq.com>
2018-07-05 09:32:27 +0800

    新增了头像组件

---
+++ notspecialyou/src/components/index/photo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="box">
+        <img src="@/assets/222.png" alt="">  
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .box{
+        width: 100%;
+    }
+    img{
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+        display: block;
+        margin: 0 auto;
+        margin-top: 25vh;
+    }
+</style>
