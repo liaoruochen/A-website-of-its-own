b6ad96d3bc0b1cd7869809a2728313bc759cc010
liaoruochen <920037556@qq.com>
2018-07-05 09:33:38 +0800

    增加

--- notspecialyou/src/components/index/title.vue
+++ notspecialyou/src/components/index/title.vue
@@ -1,19 +1,24 @@
 <template>
     <div class="container">
-       
-            <p class="title">
-                不特别的你
-            </p>
-            <list></list>
+        <linkel></linkel>
+        <photo></photo>
+        <p class="title">
+            不特别的你
+        </p>
+        <list></list>
     </div>
 </template>
 
 <script>
     // import top from '@/components/top-navigation/top-navigation'
+    import photo from '@/components/index/photo'
     import list from "@/components/index/list"
+    import linkel from '@/components/index/linkel'
     export default {
         components:{
-            list
+            list,
+            photo,
+            linkel
         }
     };
 </script>
@@ -23,7 +28,7 @@
     .title {
         font-size: 28px;
         text-align: center;
-        margin-top: 40vh;
+        margin-top: 30px;
     }
 
 </style>
