1a821008ee145a833d259b800dbd27f427960938
keepAwayFrom404 <xff777@qq.com>
2018-07-06 22:25:44 +0800

    新建main组件

---
+++ notspecialyou/src/components/main/main.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="main">
+        <div class="main-inner">
+            <div class="content">
+                <div class="post-tags">
+                    <div class="tags-title">
+                        目前共计25个标签
+                    </div>
+                    <div class="tags-list">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- notspecialyou/src/router/index.js
+++ notspecialyou/src/router/index.js
@@ -1,18 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/views/index'
+import tags from '@/views/tags'
 
-import Footers from '@/components/Footers'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path:"/",
-      component:index
-    },{
-      path: '/',
-      component: Footers
+      component:tags,
     }
   ]
 })
