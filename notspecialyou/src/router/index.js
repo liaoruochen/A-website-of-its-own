39f778f8ee098af2cc4e8275a39941403f3bb5e2
liaoruochen <920037556@qq.com>
2018-07-16 23:33:47 +0800

    增加了登录路由

--- notspecialyou/src/router/index.js
+++ notspecialyou/src/router/index.js
@@ -2,18 +2,27 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/views/index'
 import about from '@/views/about'
+import login from '@/views/login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
+      name: 'index',
       path: '/',
       component: index
     },
     {
+      name: 'about',
       path: '/about',
       component: about
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: login
     }
   ]
 })
