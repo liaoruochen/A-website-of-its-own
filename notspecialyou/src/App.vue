8c04cc38f75652abd0ee2a9915718c69e7da3326
liaoruochen <920037556@qq.com>
2018-07-16 23:37:24 +0800

    增加了transition渐变效果

--- notspecialyou/src/App.vue
+++ notspecialyou/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -10,7 +12,17 @@ export default {
 }
 </script>
 
-<style>
- @import './common/style/base.css';
+<style lang="stylus">
+@import './common/style/base.css';
+
+.fade-enter
+  opacity 0
+.fade-enter-active
+  transition opacity 0.5s
+.fade-leave
+  opacity 1
+.fade-leave-active
+  opacity 0
+  transition opacity 0.5s 
 
 </style>
