eebed819e366670ec3a2e8e732edcfeeca8f192f
liaoruochen <920037556@qq.com>
2018-07-20 23:35:41 +0800

    修改了封装的验证函数

--- notspecialyou/src/func/func.js
+++ notspecialyou/src/func/func.js
@@ -1,7 +1,9 @@
-export const check = function (admin, password, callback, error) {
-  if (admin === 'admin' && password === '111111') {
-    callback()
-  } else if (admin && password && (admin !== 'admin' || password !== '111111')) {
-    error()
+export const check = function (msg, success, nameErr, passErr) {
+  if (msg.data === 'success') {
+    success()
+  } else if (msg.data === 'nameErr') {
+    nameErr()
+  } else if (msg.data === 'passErr') {
+    passErr()
   }
 }
