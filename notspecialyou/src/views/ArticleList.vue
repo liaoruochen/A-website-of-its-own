88943fb20e3af49f4270b56e46fa55d1f21e68d2
liaoruochen <920037556@qq.com>
2018-07-24 23:25:49 +0800

    对文章列表界面进行了完善

--- notspecialyou/src/views/ArticleList.vue
+++ notspecialyou/src/views/ArticleList.vue
@@ -1,53 +1,36 @@
 <template>
-  <div>
+  <div class="box">
     <top-navigation style="background-color: #63c1ff;"></top-navigation>
     <header-body></header-body>
-    <div class="article">
-      <div class="art-list">
-        <div class="list-top">
-          <div class="author-img">
-            <img class="photo" src="@/assets/222.png" alt="">
-            <span>不特别的你</span>
-            <img style="width:21px;height:21px;margin-left: 30px" src="@/assets/time.png" alt="">
-            <span class="time">一个月前</span>
-          </div>
-        </div>
-        <img width="80%" height="250px" src="https://user-gold-cdn.xitu.io/2018/6/8/163dd6e704c4d213?imageView2/1/w/1304/h/734/q/85/format/webp/interlace/1" alt="">
-        <h2>让咖啡与代码相伴</h2>
-        <h5>程序猿最幸福的事情可能就是，在炎热的夏天，吹着空调，喝着咖啡，敲着代码。由于这几天刚接触了微信小程序</h5>
-      </div>
-    </div>
+    <Article :List="articleList" style="margin-top:40px"></Article>
   </div>
 </template>
 
 <script>
+import { articleList } from '@/api/getData.js'
 import topNavigation from '@/components/top-navigation/top-navigation'
 import headerBody from '@/components/header/header'
+import Article from '@/components/Article/Article'
 export default {
+  async created () {
+    await articleList().then( (msg) => {
+      this.articleList = msg.data
+    })
+    console.log(this.articleList)
+  },
+  data () {
+    return {
+      articleList: null
+    }
+  },
   components: {
     headerBody,
-    topNavigation
+    topNavigation,
+    Article
   }
 }
 </script>
-
 <style lang="stylus" scoped>
-.article
-  width 100%
-  .art-list
-    width 70%
-    height 300px
-    margin 0 auto
-    .list-top
-      height 50px
-      .author-img
-        display flex
-        align-items center
-        width 300px
-        .photo
-          width 50px
-          height 50px
-          margin-right 5px
-        .time
-          margin-left 5px
+.box
+  background-color #f4f5f5
 </style>
