865cd8d53239d60534811e1e8a654ab91ef876e8
liaoruochen <920037556@qq.com>
2018-07-27 13:18:27 +0800

    修改了请求地址，请求的服务器上的地址

--- notspecialyou/src/api/getData.js
+++ notspecialyou/src/api/getData.js
@@ -1,20 +1,20 @@
 import axios from 'axios'
 
 export const postArticle = (data) => new Promise((resolve, reject) => {
-  axios.post('http://127.0.0.1:3000/user/article', data).then(res => {
+  axios.post('http://www.liaoruochen.cn:3000/user/article', data).then(res => {
     resolve(res)
   })
 })
 
 export const checkForm = (userinfo) => new Promise((resolve, reject) => {
-  axios.post('http://127.0.0.1:3000/user/login', userinfo).then((res) => {
+  axios.post('http://www.liaoruochen.cn:3000/user/login', userinfo).then((res) => {
     resolve(res)
     // console.log(res)
   })
 })
 
 export const articleList = () => new Promise((resolve, reject) => {
-  axios.get('http://127.0.0.1:3000/user/articleList').then((res) => {
+  axios.get('http://www.liaoruochen.cn:3000/user/articleList').then((res) => {
     resolve(res)
   })
 })
