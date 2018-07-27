c3885f380fe77f6f110d93fbac9c8b3fb69c4c58
liaoruochen <920037556@qq.com>
2018-07-27 13:19:27 +0800

    懒加载了路由

--- notspecialyou/src/router/index.js
+++ notspecialyou/src/router/index.js
@@ -1,13 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '@/views/index'
-import about from '@/views/about'
-import login from '@/views/login'
-import system from '@/views/system'
-import news from '@/views/News'
-import NotFound from '@/views/404'
-import article from '@/views/article'
-import articlelist from '@/views/ArticleList'
+
+// import index from '@/views/index'
+// import about from '@/views/about'
+// import login from '@/views/login'
+// import system from '@/views/system'
+// import news from '@/views/News'
+// import NotFound from '@/views/404'
+// import article from '@/views/article'
+// import articlelist from '@/views/ArticleList'
+// import jl from '@/views/jl'
+
+const index = resolve => require(['@/views/index'], resolve)
+const about = resolve => require(['@/views/about'], resolve)
+const login = resolve => require(['@/views/login'], resolve)
+const system = resolve => require(['@/views/system'], resolve)
+const news = resolve => require(['@/views/News'], resolve)
+const NotFound = resolve => require(['@/views/404'], resolve)
+const article = resolve => require(['@/views/article'], resolve)
+const articlelist = resolve => require(['@/views/articlelist'], resolve)
+const jl = resolve => require(['@/views/jl'], resolve)
 
 Vue.use(Router)
 
@@ -52,6 +64,10 @@ export default new Router({
     {
       path: '*',
       component: NotFound
+    },
+    {
+      path: '/jianli',
+      component: jl
     }
 
   ]
